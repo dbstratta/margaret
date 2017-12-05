4661c8468ad8a53e3cce605ea4d8de84fab6d17f
Diego Stratta <strattadb@gmail.com>
2017-12-05 00:08:44 -0300

    Move resolve_type to bottom of interface (#22)

--- projects/api/lib/margaret_web/schema/starrable_types.ex
+++ projects/api/lib/margaret_web/schema/starrable_types.ex
@@ -10,13 +10,10 @@ defmodule MargaretWeb.Schema.StarrableTypes do
 
   interface :starrable do
     field :id, non_null(:id)
-    resolve_type fn
-      _, _ ->
-        nil
-    end
 
     @desc "The stargazers of the starrable."
     field :stargazers, :user_connection
+
     resolve_type fn
       _, _ ->
         nil
